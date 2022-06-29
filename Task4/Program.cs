8356644b4c27f4a671e93903472aef8f5d0d3d6e
Mars1501 <87420575+Mars1501@users.noreply.github.com>
2022-06-29 19:25:36 +0300

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for(i = 1; i <= num; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
