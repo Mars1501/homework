b51f83feb12d7a85ba4bea0ff7c4a25897610251
Denis <den.batuev99@gmail.com>
2022-06-29 11:28:43 +0300

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(max < numberB)
+{
+    max = numberB;
+}
+if(max < numberC)
+{
+    max = numberC;
+}
+
+Console.WriteLine(" Максимальное число  (" +numberA+ " , " +numberB+ " , " +numberC+ ") является " +max);
