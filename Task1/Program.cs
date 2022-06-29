ed88fedd4c3be2246309df841e435d4ad528a9e1
Denis <den.batuev99@gmail.com>
2022-06-29 11:05:09 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine(" Число " +numberA+  " больше " + numberB);
+}
+else
+{
+    Console.WriteLine(" Число " +numberB+  " больше " +numberA);
+}
